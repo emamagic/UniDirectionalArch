ecfd804a24c2dd984587a72dc09a4457cf0eb243
emamagic <developermagic7@gmail.com>
2021-08-16 11:57:05 +0430

    first implementation

--- app/src/main/java/com/emamagic/mvi/bansa/ApiMiddleware.kt
+++ app/src/main/java/com/emamagic/mvi/bansa/ApiMiddleware.kt
@@ -1,33 +1,32 @@
 package com.emamagic.mvi.bansa
 
 class GifMiddleware : Middleware<ApplicationState> {
-    private var currentRequest = Subscriptions.empty()
 
     override fun dispatch(store: Store<ApplicationState>, action: Action, next: NextDispatcher) {
-        when (action) {
-            is FETCH_NEXT_PAGE -> {
-                currentRequest = fetchTrendingGifs(
-                    store.state.pagination.offset,
-                    store.state.pagination.count).subscribe({
-                    next(FETCH_NEXT_PAGE_COMPLETED(it))
-                }, {
-                    throw it
-                })
-
-                next(FETCH_NEXT_PAGE_STARTED)
-            }
-            is REFRESH -> {
-                currentRequest.unsubscribe()
-
-                currentRequest = fetchTrendingGifs().subscribe({
-                    next(REFRESH_COMPLETED(it))
-                }, {
-                    throw it
-                })
-
-                next(REFRESH_STARTED)
-            }
-            else -> next(action)
-        }
+//        when (action) {
+//            is FETCH_NEXT_PAGE -> {
+//                currentRequest = fetchTrendingGifs(
+//                    store.state.pagination.offset,
+//                    store.state.pagination.count).subscribe({
+//                    next(FETCH_NEXT_PAGE_COMPLETED(it))
+//                }, {
+//                    throw it
+//                })
+//
+//                next(FETCH_NEXT_PAGE_STARTED)
+//            }
+//            is REFRESH -> {
+//                currentRequest.unsubscribe()
+//
+//                currentRequest = fetchTrendingGifs().subscribe({
+//                    next(REFRESH_COMPLETED(it))
+//                }, {
+//                    throw it
+//                })
+//
+//                next(REFRESH_STARTED)
+//            }
+//            else -> next(action)
+//        }
     }
 }
